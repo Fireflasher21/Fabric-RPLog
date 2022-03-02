d2c974dbf1d02e12275a28d496117a56093958df
BuildTools <unconfigured@null.spigotmc.org>
2022-03-02 08:54:59 +0100

    Version 3.1, Fixed Dependency (1.17)

--- src/main/java/fireflasher/fabricrplog/config/Optionsscreen.java
+++ src/main/java/fireflasher/fabricrplog/config/Optionsscreen.java
@@ -8,7 +8,6 @@ import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
-import org.checkerframework.checker.units.qual.C;
 
 public class Optionsscreen extends GameOptionsScreen {
 
