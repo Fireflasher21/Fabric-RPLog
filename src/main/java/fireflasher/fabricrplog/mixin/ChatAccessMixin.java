20c394d4d71d0142e0c085c077888e39262d62f1
Fireflasher21 <fireflasher@proton.me>
2023-04-21 17:32:43 +0200

    Updated Chat Log Method in Mixin

--- src/main/java/fireflasher/fabricrplog/mixin/ChatAccessMixin.java
+++ src/main/java/fireflasher/fabricrplog/mixin/ChatAccessMixin.java
@@ -5,16 +5,20 @@ import fireflasher.fabricrplog.ChatLogger;
 import net.minecraft.client.network.message.MessageHandler;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.time.Instant;
+import java.util.logging.Logger;
+
 @Mixin(MessageHandler.class)
 public abstract class ChatAccessMixin {
-    @Inject(method = "onChatMessage", at = @At("HEAD"), cancellable = true)
-    private void onChatMessage(SignedMessage message, GameProfile sender, MessageType.Parameters params, CallbackInfo ci)  {
-        String content = message.signedBody().content();
+    @Inject(method = "addToChatLog(Lnet/minecraft/text/Text;Ljava/time/Instant;)V", at = @At("HEAD"), cancellable = true)
+    private void onChatMessage(Text message, Instant timestamp, CallbackInfo ci)  {
+        String content = message.getString();
         ChatLogger.chatFilter(content);
     }
 }
